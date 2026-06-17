bc6fb0388e202c367107e5a4699cad34939168a0
kerols badr tawfik zaki <59099087+krrish-dev@users.noreply.github.com>
2026-06-17 07:38:52 +0300

    Deduplicate generated and content location data

--- src/lib/locations.ts
+++ src/lib/locations.ts
@@ -4,5 +4,16 @@ import { ADDITIONAL_LOCATIONS } from '../data/additionalLocations';
 export async function getAllLocations() {
   const contentLocations = await getCollection('locations');
   const additionalLocations = ADDITIONAL_LOCATIONS.map((data) => ({ data }));
-  return [...contentLocations, ...additionalLocations];
+
+  const locationsBySlug = new Map<string, { data: any }>();
+
+  for (const location of additionalLocations) {
+    locationsBySlug.set(`${location.data.stateSlug}/${location.data.citySlug}`, location);
+  }
+
+  for (const location of contentLocations) {
+    locationsBySlug.set(`${location.data.stateSlug}/${location.data.citySlug}`, location);
+  }
+
+  return Array.from(locationsBySlug.values());
 }
